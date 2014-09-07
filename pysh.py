493448145a1d3e83188592ec7bad8ec64e9580e7
Ben Doan <ben@bendoan.me>
2014-09-07 18:46:04 -0500

    added support for local executables and the _ function for executing commands with symbols

--- pysh.py
+++ pysh.py
@@ -4,24 +4,31 @@ import sys
 
 from os import path
 
-def is_executable(f):
+def _is_executable(f):
     return path.isfile(f) and os.access(f, os.X_OK)
+
+def _run_program(name, *args):
+    args = [name] + list(args)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return [x.decode(sys.getdefaultencoding()) for x in p.communicate()]
+
+def _get_function(name):
+    return lambda *args: _run_program(name, *args)
 
 def get_programs():
     programs = []
+    os.environ['PATH'] += os.pathsep + os.getcwd()
     for p in os.environ['PATH'].split(os.pathsep):
         if path.isdir(p):
             for f in os.listdir(p):
-                if not is_executable(f):
+                if _is_executable(path.join(p, f)):
                     programs.append(f)
     return programs
 
-def get_function(name):
-    def f(*args):
-        args = [name] + list(args)
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        return p.communicate()
-    return f
+
 valid_names = (x for x in get_programs() if x.isalpha())
-for f in valid_names:
-    globals()[f] = get_function(f)
+for f in get_programs():
+    if f.isalpha():
+        globals()[f] = _get_function(f)
+
+globals()["_"] = lambda name, *args: _run_program(name, *args)
