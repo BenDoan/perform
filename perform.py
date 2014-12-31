d4588a0e6abab01b46b7950e59a2c434d44bf4ab
Ben Doan <ben@bendoan.me>
2014-12-31 16:53:24 -0600

    updated doc formatting

--- perform.py
+++ perform.py
@@ -4,54 +4,42 @@ Perform is a python module for calling processes in a simple and easy way.  Each
 ##usage:
 - To call a program:
 
-```python
-import perform
-stdout = perform.ls()
-```
+    import perform
+    stdout = perform.ls()
 
 - To pass arguments to a program:
 
-```python
-stdout = perform.git("ls-files", "-m")
-```
+    stdout = perform.git("ls-files", "-m")
 
 - To call a program that contains symbols in its name:
 
-```python
-stdout = perform._("pip2.7", "install", "perform")
-```
+    stdout = perform._("pip2.7", "install", "perform")
 
 - To get extra information from a program:
 
-```python
-command_object = perform.ls(return_object=True)
+    command_object = perform.ls(return_object=True)
 
-stdout = command_object.stdout
-stderr = command_object.stderr
-stdout = command_object.stdout
-```
+    stdout = command_object.stdout
+    stderr = command_object.stderr
+    stdout = command_object.stdout
 
 - To call a command in the shell:
 
-```python
-print(perform._("ls | grep 'py'", shell=True))
-```
+    print(perform._("ls | grep 'py'", shell=True))
 
 ##more examples
 
-```python
-import perform
+    import perform
 
-stdout = perform.ls()
+    stdout = perform.ls()
 
-print(perform.git("ls-files", "-m"))
+    print(perform.git("ls-files", "-m"))
 
-print(perform._("pip2.7", "install", "perform"))
+    print(perform._("pip2.7", "install", "perform"))
 
-stdout = perform.raspistill("-o ~/image.jpg")
+    stdout = perform.raspistill("-o ~/image.jpg")
 
-print(perform.python("-c", "import perform;print(perform.echo('hello'))")
-```
+    print(perform.python("-c", "import perform;print(perform.echo('hello'))")
 """
 from __future__ import unicode_literals
 
@@ -65,7 +53,7 @@ from os import path
 from functools import partial
 
 __author__ = "Ben Doan <ben@bendoan.me>"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 class StandardErrorException(Exception):
     pass
